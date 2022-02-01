55b964ffdb209fa126d893b00ad9b551a34e4943
nik-kita <nik.moiseienko@gmail.com>
2022-02-01 22:29:48 +0200

    basic

---
+++ generators/app/index.js
@@ -0,0 +1,41 @@
+const Generator = require('yeoman-generator');
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+
+    this.argument('appName', { type: String, required: true });
+  }
+
+  initializing() {
+
+  }
+
+  prompting() {
+
+  }
+
+  configuring() {
+
+  }
+
+  default() {
+
+  }
+
+  writing() {
+
+  }
+
+  conflicts() {
+
+  }
+
+  install() {
+
+  }
+
+  end() {
+
+  }
+};
